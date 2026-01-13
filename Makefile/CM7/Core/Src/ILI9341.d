2c8acb3e85e59286a6bb09db06bfc8a877b508b2
George <georgeumb04@gmail.com>
2026-01-13 02:43:59 -0800

    Basic GUI Setup

--- Makefile/CM7/Core/Src/ILI9341.d
+++ Makefile/CM7/Core/Src/ILI9341.d
@@ -21,6 +21,9 @@ build/../../CM7/Core/Src/ILI9341.o: ../../CM7/Core/Src/ILI9341.c \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h \
+ ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc.h \
+ ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_ll_adc.h \
+ ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc_ex.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h \
@@ -28,8 +31,6 @@ build/../../CM7/Core/Src/ILI9341.o: ../../CM7/Core/Src/ILI9341.c \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h \
- ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi.h \
- ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi_ex.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h \
  ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h \
  ../../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.h \
@@ -58,6 +59,9 @@ build/../../CM7/Core/Src/ILI9341.o: ../../CM7/Core/Src/ILI9341.c \
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_mdma.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_exti.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_cortex.h:
+../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc.h:
+../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_ll_adc.h:
+../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_adc_ex.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_flash_ex.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_hsem.h:
@@ -65,8 +69,6 @@ build/../../CM7/Core/Src/ILI9341.o: ../../CM7/Core/Src/ILI9341.c \
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_i2c_ex.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_pwr_ex.h:
-../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi.h:
-../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_spi_ex.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart.h:
 ../../Drivers/STM32H7xx_HAL_Driver/Inc/stm32h7xx_hal_uart_ex.h:
 ../../Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.h:
---
+++ Makefile/CM7/build/frogfs.d
@@ -0,0 +1,7 @@
+build/frogfs.o: ../../Drivers/lvgl/src/libs/frogfs/src/frogfs.c \
+ ../../Drivers/lvgl/src/libs/frogfs/src/../../../lv_conf_internal.h \
+ ../../Drivers/lvgl/src/libs/frogfs/src/../../../lv_conf_kconfig.h \
+ ../../Drivers/lvgl/src/libs/frogfs/src/../../../../../lv_conf.h
+../../Drivers/lvgl/src/libs/frogfs/src/../../../lv_conf_internal.h:
+../../Drivers/lvgl/src/libs/frogfs/src/../../../lv_conf_kconfig.h:
+../../Drivers/lvgl/src/libs/frogfs/src/../../../../../lv_conf.h:
